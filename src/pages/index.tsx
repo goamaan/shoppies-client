1c94921653f7f600e26fe44bd17e52425fb26f32
amaan18 <amaangokak18@gmail.com>
2021-04-28 12:40:12 +0530

    init nextjs chakra typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "../components/Container";
+import { Main } from "../components/Main";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+
+const Index = () => (
+  <Container height="100vh">
+    <Main>
+      <DarkModeSwitch />
+    </Main>
+  </Container>
+);
+
+export default Index;
